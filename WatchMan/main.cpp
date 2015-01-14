4619e438468b031b0f1de952c23fef05f5912bab
Hoang Nguyen <hvn1@hvn1-Inspiron-N5040.(none)>
2015-01-14 12:02:29 -0500

    Improved adjacency list representation.

--- WatchMan/main.cpp
+++ WatchMan/main.cpp
@@ -1,5 +1,15 @@
 #include "graph.hpp"
+#include "bfs.hpp"
+#include <string>
+
+using namespace std;
 
 int main() {
+	using Vint = Vertex<int>;
+
+	Vint v0(0);
+	Vint v1(1);
+	Vint v2(2);
+
 	return 0;
 }
