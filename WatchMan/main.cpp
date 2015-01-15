8ae519c8ca9d284377699280f0161c7e99d6de09
Hoang Nguyen <hvn1@hvn1-Inspiron-N5040.(none)>
2015-01-15 13:52:44 -0500

    Changed implementation of adj list to use pointers. Finihed BFS implementation like the book.

--- WatchMan/main.cpp
+++ WatchMan/main.cpp
@@ -1,6 +1,7 @@
 #include "graph.hpp"
 #include "bfs.hpp"
 #include <string>
+#include <unordered_set>
 
 using namespace std;
 
@@ -9,20 +10,30 @@ int main() {
 	using Eint = Edge<int>;
 	using al = AdjacencyList<Eint>;
 
-	Vint a(0);
-	Vint b(1);
-	Vint c(2);
-	Vint d(3);
+	Vint* a = new Vint(0);
+	Vint* a1 = new Vint(0);
+	Vint* b = new Vint(1);
+	Vint* c = new Vint(2);
+	Vint* d = new Vint(3);
 
 	//replicate the same graph as in the example on the assignment sheet
 	Graph<int> g;
+	g.add_edge(a, d, 2);
 	g.add_edge(a, b, 3);
 	g.add_edge(a, c, 6);
 	g.add_edge(b, c, 2);
-	g.add_edge(a, d, 8);
 	g.add_edge(c, d, 1);
 
-	g.print();
-	
+	// g.print();
+
+	// ordered_set<Vint> os;
+	// os.add(a);
+	// if(os.contains(a1)) {
+	// 	std::cout << "CONTAINS";
+	// }
+
+	std::cout << "PATH\n";
+	auto path = bfs(g, b, d);
+
 	return 0;
 }
