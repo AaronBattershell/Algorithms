8176532c47b5a37e0b889b1c225cee12df2f66bb
aaronbattershell <soccerman0010@gmail.com>
2015-01-26 05:14:46 -0500

    watchman.cpp  prepping

--- WatchMan/watchman.hpp
+++ WatchMan/watchman.hpp
@@ -3,14 +3,20 @@
 
 #include <string>
 #include <iostream>
+#include <vector>
 #include "intersection.hpp"
 #include "inputData.h"
 
 struct watchman {
-	inputData input;
+	public:
+		inputData input;
 
-	watchman(std::string fileName);
-	void solve();
+		watchman(std::string fileName);
+		void solve();
+
+	private:
+		void createWalls(std::vector<line> &straitWall, std::vector<arc> &curveWall);
+		void findSightLines(std::vector<line> &straitWall, std::vector<arc> &curveWall);
 };
 
 #endif
