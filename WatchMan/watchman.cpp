98ba20a676fee81e6a2f315c73ec928733d57851
Flander <flander@WhyNotZoidberg.(none)>
2015-01-28 00:57:47 -0500

    Added super source and sink nodes

--- WatchMan/watchman.cpp
+++ WatchMan/watchman.cpp
@@ -2,27 +2,37 @@
 using namespace std;
 
 watchman::watchman(string fileName)
-	:input(fileName) {
-	
-}
+	:input(fileName) 
+{ }
 
 void watchman::solve() {
 	for (int iter = 1; input.get_next_problem(); ++iter) {	
-		//input.print_wallSet(input.wall_setVector);
-    		//input.print_paintSet(input.paint_setVector);
-    		//input.print_guardSet(input.guard_setVector);
-
+		int artSum = 0;
 		vector<line> straitWall;
 		vector<arc> curveWall;
 		Graph<int> g;
 		VertexFactory<int>* vf = g.vf;
 
+		for (int i = 0 ; i < input.paint_setVector.size(); ++i) {
+			artSum += input.paint_setVector[i][2];
+		}
+
 		createWalls(straitWall, curveWall);
 		findSightLines(straitWall, curveWall, g, vf);
-		// g.print();
+		
 		Vertex<int> *src = vf->make_vertex(get_src(g).value);
 		Vertex<int> *sink = vf->make_vertex(get_sink(g).value);
+
+		cout << "===Starting Graph===\n";
+		cout << "(-1 super sorce, -2 super sink, node 0 - " << input.guard_setVector.size() - 1;
+		cout << " gaurds, nodes " << input.guard_setVector.size() << " - ";
+		cout << input.guard_setVector.size() + input.paint_setVector.size() - 1 << " paintings" << endl;
+		g.print();
+		
 		ford_fulkerson(g, src, sink);
+
+
+		delete vf;
 	}
 }	
 
@@ -49,16 +59,21 @@ void watchman::createWalls(vector<line> &straitWall, vector<arc> &curveWall) {
 void watchman::findSightLines(vector<line> &straitWall, vector<arc> &curveWall, Graph<int> &g, VertexFactory<int> *vf) {
 	using Vint = Vertex<int>;
 
+	Vint *superSource = vf->make_vertex(-1);
+	Vint *superSink = vf->make_vertex(-2);
+
 	for (int i = 0; i < input.guard_setVector.size(); ++i) {
 		for (int n = 0; n < input.paint_setVector.size(); ++n) {
 			line lineOfSight(point(input.guard_setVector[i][0], input.guard_setVector[i][1]),
 				point(input.paint_setVector[n][0], input.paint_setVector[n][1]));
 			
-			Vint* curr = vf->make_vertex(i);
+			Vint *curr = vf->make_vertex(i);
 	
 			if (clearLineOfSight(lineOfSight, straitWall, curveWall)) {
 				Vint *dest = vf->make_vertex(n + input.guard_setVector.size());
 				g.add_edge(curr, dest, 1);
+				g.add_edge(superSource, curr, input.guard_setVector[i][2]);
+				g.add_edge(dest, superSink, input.paint_setVector[n][2]); 
 			}
 		}
 	}
