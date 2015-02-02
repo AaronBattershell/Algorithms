4486aa72d0dbb1d93372a391b856189162125b59
aaronbattershell <soccerman0010@gmail.com>
2015-02-02 17:57:42 -0500

    Fixed more test cases

--- WatchMan/intersection.cpp
+++ WatchMan/intersection.cpp
@@ -4,11 +4,11 @@
 using namespace std;
 
 point::point(double x, double y)
-    : x(x), y(y)
+	: x(x), y(y)
 { }
 
 double point::dist(point p) {
-    return sqrt(pow(x - p.x, 2) + pow(y - p.y, 2));
+	return sqrt(pow(x - p.x, 2) + pow(y - p.y, 2));
 }
 
 line::line(point one, point two)
@@ -16,11 +16,16 @@ line::line(point one, point two)
 { }
 
 bool line::liesOnSegment(point p) {
-	return one.dist(p) + two.dist(p) - one.dist(two) < 0.00001;
+	return one.dist(p) + two.dist(p) - one.dist(two) < TOLLERANCE;
 }
 
 bool line::parallel(line l) {
-	return (one.x - two.x == l.one.x - l.two.x && one.y - two.y == l.one.y - l.two.y);
+	//cout << "Comparing: " << "(" << one.x << ", " << one.y << "), (" << two.x << ", " << two.y << ")" << endl;
+	//cout << "Comparing: " << "(" << l.one.x << ", " << l.one.y << "), (" << l.two.x << ", " << l.two.y << ")" << endl;
+
+	return (abs((one.y - two.y) / (one.x - two.x) - (l.one.y - l.two.y) / (l.one.x - l.two.x)) < TOLLERANCE) || 
+		(abs(one.x - two.x) < TOLLERANCE && abs(l.one.x - l.two.x) < TOLLERANCE) ||
+		((abs(one.y - two.y) < TOLLERANCE && abs(l.one.y - l.two.y) < TOLLERANCE));
 }
 
 // Returns true if two line segments intersect
@@ -35,46 +40,47 @@ bool line::intersect(arc c) {
 	return c.intersect(*this);
 }
 
-arc::arc(point one, point two, double dx, double dy) 
-	: one(one), two(two), dx(dx), dy(dy) { 
-	
+arc::arc(point one, point two, double dx, double dy)
+	: one(one), two(two), dx(dx), dy(dy) {
+
 	point centerOfGivenPoints((one.x + two.x) / 2, (one.y + two.y) / 2);
 	line lineToCenter1(point(one.x, one.y), point(one.x - dy, one.y + dx));
 	line lineToCenter2(point(centerOfGivenPoints.x, centerOfGivenPoints.y),
 		point(centerOfGivenPoints.x + (one.x - two.x), centerOfGivenPoints.y - (one.y - two.y)));
-	
-	if (lineToCenter1.parallel(lineToCenter2)) {
-			double xInt = (lineToCenter1.yInt - lineToCenter2.yInt) / (lineToCenter2.slope - lineToCenter1.slope);
-			center.x = xInt;
-			center.y = lineToCenter2.slope * xInt + lineToCenter2.yInt;
+
+	if (!lineToCenter1.parallel(lineToCenter2)) {
+		double xInt = (lineToCenter1.yInt - lineToCenter2.yInt) / (lineToCenter2.slope - lineToCenter1.slope);
+		center.x = xInt;
+		center.y = lineToCenter2.slope * xInt + lineToCenter2.yInt;
 	}
 	else {
-			center = centerOfGivenPoints;
+		center = centerOfGivenPoints;
+		//cout << center.x << ' ' << center.y << endl;
 	}
+
+	//cout << "Center: " << center.x << ' ' << center.y << endl;
 
 	radius = center.dist(one);
 }
 
 // Returns true if a line segment and arc intersect
 bool arc::intersect(line l) {
-	cout << l.slope << endl;
 	double A = pow(l.slope, 2) + 1;
 	double B = 2 * (l.slope * l.yInt - l.slope * center.y - center.x);
 	double C = pow(center.y, 2) - pow(radius, 2) + pow(center.x, 2) - 2 * l.yInt * center.y + pow(l.yInt, 2);
 	double D = pow(B, 2) - 4 * A * C;
 
 	/* Note: D < 0 : Zero intersection points
-	 *       D == 0: One intersection point
-	 *       D > 0 : Two intersection points
+	*       D == 0: One intersection point
+	*       D > 0 : Two intersection points
 	*/
 	if (D < 0) {
-		//cout << "No int: " << D << endl;
 		return false;
 	}
-	
+
 	line arcPointsLine(point(one.x, one.y), point(two.x, two.y));
 
-	if (true || D >= 0) {
+	if (D >= 0) {
 		double xInt = (-B + sqrt(D)) / 2 * A;
 		double yInt = l.slope * xInt + l.yInt;
 
@@ -83,13 +89,13 @@ bool arc::intersect(line l) {
 		double crossXInt = (arcPointsLine.yInt - dxdyIntLine.yInt) / (dxdyIntLine.slope - arcPointsLine.slope);
 		point lineInt(crossXInt, dxdyIntLine.slope * crossXInt + dxdyIntLine.yInt);
 
-		cout << "Intersection: " << xInt << ' ' << yInt << endl;
+		//cout << "Intersection: " << xInt << ' ' << yInt << endl;
 
 		if (!dxdyIntLine.liesOnSegment(lineInt) && l.liesOnSegment(point(xInt, yInt))) {
 			return true;
 		}
 	}
-	if (true || D > 0) {
+	if (D > 0) {
 		double xInt = (-B - sqrt(D)) / 2 * A;
 		double yInt = l.slope * xInt + l.yInt;
 
@@ -98,7 +104,7 @@ bool arc::intersect(line l) {
 		double crossXInt = (arcPointsLine.yInt - dxdyIntLine.yInt) / (dxdyIntLine.slope - arcPointsLine.slope);
 		point lineInt(crossXInt, dxdyIntLine.slope * crossXInt + dxdyIntLine.yInt);
 
-		cout << "Intersection: " << xInt << ' ' << yInt << endl;
+		//cout << "Intersection: " << xInt << ' ' << yInt << endl;
 
 		if (!dxdyIntLine.liesOnSegment(lineInt) && l.liesOnSegment(point(xInt, yInt))) {
 			return true;
--- WatchMan/intersection.hpp
+++ WatchMan/intersection.hpp
@@ -4,16 +4,17 @@
 #include <cmath>
 
 #define OFFSET 0.000000001
+#define TOLLERANCE 0.000001
 
 struct arc;
 
 struct point {
 	double x, y;
-	
+
 	point(double x = 0, double y = 0);
 	double dist(point p);
 };
-	
+
 struct line {
 	point one, two;
 	double slope, yInt;
@@ -24,11 +25,11 @@ struct line {
 	bool intersect(arc c);
 	bool parallel(line l);
 };
-	
+
 struct arc {
 	point one, two, center;
 	double dx, dy, radius;
-	
+
 	arc(point one, point two, double dx, double dy);
 	bool intersect(line l);
 };
