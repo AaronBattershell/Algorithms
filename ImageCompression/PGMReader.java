b19dffba40e1b8e8b96818ebe4197c9ae573985d
Hoang Nguyen <hvn1@labmaster.cs.uakron.edu>
2015-02-23 14:36:23 -0500

    Finished read pgm

--- ImageCompression/PGMReader.java
+++ ImageCompression/PGMReader.java
@@ -1,12 +1,20 @@
 import java.io.FileInputStream;
 import java.io.DataInputStream;
+import java.io.File;
 import java.util.Scanner;
 
 public class PGMReader {
     // Reads a PGM file 
     // credit to BalusC http://stackoverflow.com/questions/3639198/how-to-read-pgm-images-in-java
     // @string filePath location of the file
-    public int[][] readPGM(String filePath) throws Exception {
+    public void readPGM(String filePath) throws Exception {
+		File file = new File(filePath);
+		
+		filePath = file.getAbsolutePath();
+		
+		System.out.println(filePath);
+		System.out.flush();
+		
         FileInputStream fileInputStream = new FileInputStream(filePath);
         Scanner scan = new Scanner(fileInputStream);
         // Discard the magic number
@@ -43,12 +51,17 @@ public class PGMReader {
             }
             System.out.println();
         }
-        return data2D;
+        //return data2D;
     }
 
     public static void main(String args[]) {
         PGMReader r = new PGMReader();
-        String filePath = "pgm/test.pgm";
-
+        String filePath = "pgm/coins.ascii.pgm";
+        try {
+			r.readPGM(filePath);
+		}
+		catch(Exception ex) {
+			//ex.printStackTrace();
+		}
     }
 }
